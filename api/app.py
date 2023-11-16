51b9fbc0926dc3364ec8c04be5a104dfdac31d50
Paulo Gusmao <pvmg1@hotmail.com>
2023-11-15 21:19:56 -0300

    iniciada criação de modelo, api e front

---
+++ api/app.py
@@ -0,0 +1,24 @@
+from flask_openapi3 import OpenAPI, Info, Tag
+from flask import redirect
+from urllib.parse import unquote
+
+from sqlalchemy.exc import IntegrityError
+
+from model import Session
+# from logger import logger
+# from schemas import *
+from flask_cors import CORS
+
+# Instanciando o objeto OpenAPI
+info = Info(title="Minha API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
+CORS(app)
+
+home_tag = Tag(name="Documentação", description="Seleção de documentação: Swagger, Redoc ou RapiDoc")
+# paciente_tag = Tag(name="Paciente", description="Adição, visualização, remoção e predição de pacientes com Diabetes")
+
+@app.get('/', tags=[home_tag])
+def home():
+    """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
+    """
+    return redirect('/openapi')
